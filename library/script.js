ca0acde4aa19fb5a5ed4c1ccc7b92d3dedaa4300
dorjeezzz <dj.namjyal@gmail.com>
2024-08-14 13:31:12 -0600

    Edited javascript to include remove button and change read status

--- library/script.js
+++ library/script.js
@@ -1,4 +1,5 @@
 const library = [];
+let i = 0;
 
 function Book(name, author, pages, read){
     this.name = name;
@@ -10,11 +11,16 @@ function Book(name, author, pages, read){
 function addBookToLibrary(name, author, pages, read){
     const bk = new Book(name, author, pages, read);
     library.push(bk);
+    library[i++] = bk;
     return bk;
 }
 
 const container = document.querySelector(".cardcontainer");
 const button = document.querySelector("#pressed");
+
+function del(index){
+    library.splice(1,index);
+}
 
 button.addEventListener("click", (e)=>{
     let title = document.querySelector("#bookname").value;
@@ -28,14 +34,17 @@ button.addEventListener("click", (e)=>{
     if (read) variable_name = "Read";
     else variable_name = "Not Read";
     const bk = addBookToLibrary(title,author,pgno,read);
-    const div = document.createElement("div");
     div.classList.add("card");
-    library.forEach((book) =>{
-        div.innerHTML = `
+    library.forEach((book, idx) =>{
+        const book = document.createElement("div");
+        book.innerHTML = `
             <h3>Title: ${bk.name}</h3>
             <h4>Author: ${bk.author}</h4>
             <p>Pages: ${bk.pages}</p>
-            <p>${variable_name}</p>`
+            <p>${variable_name}</p>
+            <button onclick="del(${idx})">Remove</button>
+            <button><button>`
+        container.appendChild(book);
     });
     container.appendChild(div);
 });
